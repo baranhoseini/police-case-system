db6ec5e3b3baec7fe521892e8915fdfadeb0e16f
melinathr <melinatahery83@gmail.com>
2026-02-06 23:25:05 +0330

    report page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./pages/routes";
 import { AuthProvider } from "./features/auth/AuthContext";
 import "./styles/global.css";
+import "./styles/print.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
